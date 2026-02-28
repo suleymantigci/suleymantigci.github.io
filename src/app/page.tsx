1b3307e1e10ac6f33c276cac3b913bd0f6209413
Süleyman Tığcı <suleymantigci@gmail.com>
2026-03-01 00:10:36 +0300

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import { Container } from "@/components/Container";
 import { Hero } from "@/components/Hero";
 import { SectionTitle } from "@/components/SectionTitle";
 import { Benefits } from "@/components/Benefits";
-import { Video } from "@/components/Video";
 import { Testimonials } from "@/components/Testimonials";
 import { Faq } from "@/components/Faq";
-import { Cta } from "@/components/Cta";
 
 import { benefitOne, benefitTwo } from "@/components/data";
 export default function Home() {
@@ -23,18 +21,6 @@ export default function Home() {
 
       <Benefits data={benefitOne} />
       <Benefits imgPos="right" data={benefitTwo} />
-
-      <SectionTitle
-        preTitle="Watch a video"
-        title="Learn how to fullfil your needs"
-      >
-        This section is to highlight a promo or demo video of your product.
-        Analysts says a landing page with video has 3% more conversion rate. So,
-        don&apos;t forget to add one. Just like this.
-      </SectionTitle>
-
-      <Video videoId="fZ0D0cnR88E" />
-
       <SectionTitle
         preTitle="Testimonials"
         title="Here's what our customers said"
@@ -51,7 +37,6 @@ export default function Home() {
       </SectionTitle>
 
       <Faq />
-      <Cta />
     </Container>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,28 +18,14 @@ export const Navbar = () => {
       <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-1">
         {/* Logo  */}
         <Link href="/">
-          <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
-              <span>
-                <Image
-                  src="/img/logo.svg"
-                  width="32"
-                  alt="N"
-                  height="32"
-                  className="w-8"
-                />
-              </span>
-            <span>Nextly</span>
+          <span className="flex items-center space-x-2 text-5xl font-medium text-indigo-500 dark:text-gray-100">
+            <span>ST</span>
           </span>
         </Link>
 
         {/* get started  */}
         <div className="gap-3 nav__item mr-2 lg:flex ml-auto lg:ml-0 lg:order-2">
             <ThemeChanger />
-            <div className="hidden mr-3 lg:flex nav__item">
-              <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
-                Get Started
-              </Link>
-            </div>
         </div>
                 
         <Disclosure>
